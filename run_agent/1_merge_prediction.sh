db1ed20074a1f15d239c862d50d2e1644c3d0adf
Zhengyan Shi <zhengyanshi@gmail.com>
2025-06-27 19:55:56 -0700

    update script

--- run_agent/1_merge_prediction.sh
+++ run_agent/1_merge_prediction.sh
@@ -2,5 +2,6 @@
 
 FOLDER_PATH="trajectories/zhengyanshi@microsoft.com/default__o3__t-0.00__p-1.00__c-2.00___patch_swesmith_astropy__astropy.26d14786_ps"
 FOLDER_PATH="trajectories/zhengyanshi@microsoft.com/default__o3__t-0.00__p-1.00__c-2.00___patch_swesmith_pylint-dev__pylint.1f8c4d9e_ps"
+FOLDER_PATH="trajectories/zhengyanshi@microsoft.com/default__o1__t-0.00__p-1.00__c-2.00___patch_swesmith_pylint-dev__pylint.1f8c4d9e_ps"
 python sweagent/run/merge_predictions.py \
     ${FOLDER_PATH}
--- run_agent/2_run_evaluation.sh
+++ run_agent/2_run_evaluation.sh
@@ -17,6 +17,7 @@
 HOME_PATH="/home/zhengyanshi"
 TASK_DATA_PATH=$HOME_PATH/project/SWE-smith/logs/automated_pipeline_o3_bugs30_combos50_depth2_workers32_nbugs1_patches2_perfile2_permodule10/pylint-dev__pylint.1f8c4d9e/task_insts/pylint-dev__pylint.1f8c4d9e_ps.json
 PREDICTIONS_PATH=$HOME_PATH/project/SWE-agent/trajectories/zhengyanshi@microsoft.com/default__o3__t-0.00__p-1.00__c-2.00___patch_swesmith_pylint-dev__pylint.1f8c4d9e_ps/preds.json
+PREDICTIONS_PATH=$HOME_PATH/project/SWE-agent/trajectories/zhengyanshi@microsoft.com/default__o1__t-0.00__p-1.00__c-2.00___patch_swesmith_pylint-dev__pylint.1f8c4d9e_ps/preds.json
 NUM_WORKERS=64
 python $HOME_PATH/repo/SWE-bench/swebench/harness/run_evaluation_1r1m.py \
     --dataset_name $TASK_DATA_PATH \
