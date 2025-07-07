7647bb57f01c1db4844dd18d50a7e5926513c3c3
ZhengyanShi <michaelszx117@gmail.com>
2025-07-06 20:55:55 -0400

    train models with trorch tune

---
+++ run_agent/4_run_train_torchtune.sh
@@ -0,0 +1,30 @@
+# llamafactory-cli train run_agent/llama_factory_config/train_1r1m_openhands_05B_agent.yaml output_dir=test_1r1m
+# amlt workspace add $WORKSPACE_NAME --resource-group $RESOURCE_GROUP_NAME --subscription $SUBSCRIPTION_NAME
+
+# Load environment variables from .env file
+set -a  # automatically export all variables
+source .env
+set +a  # turn off automatic export
+
+DATA_FOLDER="data/automated_pipeline_o3_bugs30_combos50_depth2_workers32_nbugs1_patches2_perfile2_permodule10"
+DATASETS=("pylint-dev__pylint.1f8c4d9e.json" "astropy__astropy.26d14786.json")
+# MODEL="Qwen/Qwen2.5-Coder-32B-instruct"
+MODEL="SWE-bench/SWE-agent-LM-32B"
+MODEL_SLUG=$(echo "$MODEL" | sed 's|.*/||')
+EPOCHS=(3)
+LEARNING_RATES=(5e-5)
+CONTEXT_LENGTH=32768
+
+for EPOCH in "${EPOCHS[@]}"; do
+    for DATASET in "${DATASETS[@]}"; do
+        for LEARNING_RATE in "${LEARNING_RATES[@]}"; do
+            JOB_NAME=":tt_${MODEL_SLUG}_cl${CONTEXT_LENGTH}_lr${LEARNING_RATE}_ep${EPOCH}_${DATASET}"
+
+            amlt run run_agent/amlt_config/torch_run_32B.yaml :torch_run_32B_1r1m=$JOB_NAME \
+                -t $VC_NAME \
+                -w $WORKSPACE_NAME \
+                -x "dataset.data_files=$DATA_FOLDER/$DATASET epochs=$EPOCH optimizer.lr=$LEARNING_RATE tokenizer.max_seq_len=$CONTEXT_LENGTH exp_name=$JOB_NAME" \
+                -y -d "Train Qwen 3 Coder 32B Model on Froggy dataset using torch tune"
+        done
+    done
+done
