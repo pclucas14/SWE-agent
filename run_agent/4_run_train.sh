97c37e17b79328940875fce473e79e932ad2688d
ZhengyanShi <michaelszx117@gmail.com>
2025-07-24 21:38:15 -0400

    update

--- run_agent/4_run_train.sh
+++ run_agent/4_run_train.sh
@@ -6,19 +6,27 @@ set -a  # automatically export all variables
 source .env
 set +a  # turn off automatic export
 
-DATASET="pylint-dev__pylint.1f8c4d9e"
-DATASET="astropy__astropy.26d14786"
+# DATASETS=("pylint-dev__pylint.1f8c4d9e" "astropy__astropy.26d14786")
+DATASETS=("astropy__astropy.26d14786_submit")
+
 # MODEL="Qwen/Qwen2.5-Coder-32B-instruct"
 MODEL="SWE-bench/SWE-agent-LM-32B"
 MODEL_SLUG=$(echo "$MODEL" | sed 's|/|--|g')
-EPOCH=2
-LEARNING_RATE=1e-5
+EPOCHS=(3)
+LEARNING_RATES=(1e-7)
 CONTEXT_LENGTH=32768
-JOB_NAME=":run_1r1m_32B=run_1r1m_32B_$DATASET"
-EXPERIMENT_NAME="o3_${MODEL_SLUG}_cl${CONTEXT_LENGTH}_lr${LEARNING_RATE}_ep${EPOCH}_$DATASET"
+WANDB_PROJECT=msrmtl-zhengyan
 
-amlt run run_agent/amlt_config/run_1r1m_32B.yaml $JOB_NAME $EXPERIMENT_NAME \
-    -t $VC_NAME \
-    -w $WORKSPACE_NAME \
-    -x "dataset=$DATASET model_name_or_path=$MODEL num_train_epochs=$EPOCH learning_rate=$LEARNING_RATE cutoff_len=$CONTEXT_LENGTH" \
-    -y -d "Train ${MODEL} on 1R1M dataset"
+for EPOCH in "${EPOCHS[@]}"; do
+    for DATASET in "${DATASETS[@]}"; do
+        for LEARNING_RATE in "${LEARNING_RATES[@]}"; do
+            JOB_NAME="lf_${MODEL_SLUG}_cl${CONTEXT_LENGTH}_lr${LEARNING_RATE}_ep${EPOCH}_${DATASET}"
+
+            amlt run run_agent/amlt_config/run_1r1m_32B.yaml :run_1r1m_32B=$JOB_NAME \
+                -t $VC_NAME \
+                -w $WORKSPACE_NAME \
+                -x "dataset=$DATASET model_name_or_path=$MODEL num_train_epochs=$EPOCH learning_rate=$LEARNING_RATE cutoff_len=$CONTEXT_LENGTH run_name=$JOB_NAME" \
+                -y -d "Train ${MODEL} on 1R1M dataset"
+        done
+    done
+done
